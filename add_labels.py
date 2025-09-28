505b96a4aff2c88574d966a3a4a8a6ba4b6fda1f
Tislander1 <blurryprojection@gmail.com>
2025-09-28 04:27:09 -0600

    update dataset

---
+++ add_labels.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+directory = "time_series_raw"
+for filename in os.listdir(directory):
+    result = pd.read_table(directory + '/' + filename, sep='\s+')
+    result.columns = ['Decimal_Year',
+                      'East (m)', 'North (m)', 'Vertical (m)',
+                      'East_sig (m)', 'North_sig (m)', 'Vertical_sig (m)',
+                      'E_N_cor', 'E_V_cor', 'N_V_cor',
+                      'Time (sec past J2000)',
+                      'Time_Year', 'Time_MM', 'Time_DD', 'Time_HR', 'Time_MN', 'Time_SS']
+    filepath_to_write = (directory + '/' + filename) + '.csv'
+    result.to_csv(filepath_to_write)
+    x = 2
