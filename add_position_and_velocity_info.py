4396e1434a7793b7fb0611a0ecde151fc9b9d287
Tislander1 <blurryprojection@gmail.com>
2025-09-28 04:49:33 -0600

    add cartesian positions and velocities.csv

---
+++ add_position_and_velocity_info.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+with open('cartesian_positions_and_velocities.html') as f:
+    data = f.readlines()
+    data = data[6:] # get rid of first lines
+all_fields = []
+for ix in range(len(data)):
+    line = data[ix]
+    if ix % 2 == 0:
+        fields1 = line.split()
+        x = 2
+    else:
+        fields2 = line.split()
+        fields = fields1 + fields2
+        all_fields.append(fields)
+df = pd.DataFrame(all_fields)
+df.columns = ['Station', 'POS',
+              'X pos (mm)', 'Y pos (mm)', 'Z pos (mm)',
+              'Err X pos (mm)', 'Err Y pos (mm)', 'Err Z pos (mm)',
+              'Station1', 'VEL', 'X vel (mm/yr)', 'Y vel (mm/yr)', 'Z vel (mm/yr)',
+              'Err X vel (mm/yr)', 'Err Y vel (mm/yr)', 'Err Z vel (mm/yr)']
+df = df.drop(columns = ['POS', 'VEL', 'Station1'])
+df.to_csv('cartesian_positions_and_velocities.csv')
+x = 2
